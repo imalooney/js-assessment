36896e21f67f97baf4ea155eb8d2c4d5e2425c89
Elaine Looney <imalooney@gmail.com>
2014-02-15 16:34:52 -0600

    make bestPractices passing

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,29 +8,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     globals : function() {
-      myObject = {
+      var myObject = {
         name : 'Jory'
       };
-
       return myObject;
     },
 
     functions : function(flag) {
       if (flag) {
-        function getValue() { return 'a'; }
+        getValue = function() { return 'a'; }
       } else {
-        function getValue() { return 'b'; }
+        getValue = function() { return 'b'; }
       }
-
       return getValue();
     },
 
     parseInt : function(num) {
-      return parseInt(num);
+      return parseInt(num, [10]);
     },
 
     identity : function(val1, val2) {
-
+      return (val1 === val2) ? true : false;
     }
   };
 });
