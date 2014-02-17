1fb445d46ff9f1415a7a0ea67b5496aba81f74d0
Elaine Looney <imalooney@gmail.com>
2014-02-16 18:50:49 -0600

    update Functions with some failing test

--- app/functions.js
+++ app/functions.js
@@ -3,19 +3,28 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn.apply(null, arr);
     },
 
     speak : function(fn, obj) {
-
+        return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+        return function(str2) {
+            return str + ', ' + str2;
+        } 
     },
 
     makeClosures : function(arr, fn) {
-
+        var closured = [];
+        var makeFunc = function(val) {
+            return function() { return fn(val); };
+        };
+        for (var i=0; i<arr.length; i++) {
+            closured.push(makeFunc(arr[i]));
+        }
+        return closured;
     },
 
     partial : function(fn, str1, str2) {
@@ -27,7 +36,9 @@ define(function() {
     },
 
     callIt : function(fn) {
-
+        // for (var i=0; i<arguments.length; i++) {
+        //     return fn(arguments[i]);
+        // }
     },
 
     partialUsingArguments : function(fn) {
@@ -35,6 +46,11 @@ define(function() {
     },
 
     curryIt : function(fn) {
+        // if (arguments.length < 1) {
+        //     return fn;
+        // } else if (arguments.length < 2) {
+        //     return fn arguments[0] / arguments[1];
+        // }
 
     }
   };
