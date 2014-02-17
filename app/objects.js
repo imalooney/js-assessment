408a6004b65cf4cf7a16b4161d151494914379bb
Elaine Looney <imalooney@gmail.com>
2014-02-16 18:34:55 -0600

    make Objects passing

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+      var keys = Object.keys(obj);
+      for ( var i=0; i<keys.length; i++ ) {
+        keys[i] = keys[i] + ': ' + obj[keys[i]];
+      }
+      return keys;
     }
   };
 });
