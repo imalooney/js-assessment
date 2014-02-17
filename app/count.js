2b0e7653b94786557065b52e3f82eae03265be2d
Elaine Looney <imalooney@gmail.com>
2014-02-16 18:43:13 -0600

    update Count though still failing

--- app/count.js
+++ app/count.js
@@ -3,6 +3,29 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+
+      console.log(start);
+      var intervalID = function() {
+        setInterval ( increment, 100 );
+      };
+
+      intervalID();
+
+      function increment() {
+        if ( start <= end ) {
+          start++;
+          console.log(start);
+        } else {
+          clearInterval(intervalID);
+        }
+      }
+
+      return { 
+        cancel: function() {
+          clearInterval(intervalID);
+        }
+      };
+
     }
   };
 });
