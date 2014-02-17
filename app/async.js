2e195aa2d2b7932fcec84ad47b3d26117d889ad7
Elaine Looney <imalooney@gmail.com>
2014-02-16 18:39:47 -0600

    add changes to Async still failing

--- app/async.js
+++ app/async.js
@@ -3,11 +3,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+      // .then
 
+      // .fail
+
+      // .done();
     },
 
     manipulateRemoteData : function(url) {
-
+      $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+          return data;
+        }, 
+        error: function(xhr, status) {
+          console.log("there was an error");
+        }
+      });
     }
   };
 });
